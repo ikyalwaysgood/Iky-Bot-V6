1361d7f89a75f6ab5c1a20365bd3da89622fc2f7
ཌ✼ʟᴇᴠɪ-ʏᴀɴ-xʜɪᴀᴏ✼ད <111189476+ImleviOfc@users.noreply.github.com>
2022-09-10 13:15:59 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `〔 Dᴏɴ'ᴛ ғᴏʀɢᴇᴛ Dᴏɴᴀᴛɪᴏɴ 〕
 
 ››╭─〘 *Donasi* 〙
-╭╡📮: Donasi ngab jgan gunain doang.
+╭╡📮: Donasi seikhlas nya:) 
 │┝‷✧ *Ovo:* 085172446692
 │┝‷✧ *Dana:* 085172446692
 │┝‷✧ *Pulsa:* 085172446692
 │╰───···─────
-│⁺◛˖ Ingin Donasi? Chat nomor owner
+│⁺◛˖ arigatou gozaimasu
 ╰──────────···───╮
 `
 const templateButtons = [
