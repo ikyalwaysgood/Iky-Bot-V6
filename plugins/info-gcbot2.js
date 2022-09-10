27b3155f0ce68064e03467b219c2e73a0dc5ee28
ཌ✼ʟᴇᴠɪ-ʏᴀɴ-xʜɪᴀᴏ✼ད <111189476+ImleviOfc@users.noreply.github.com>
2022-09-10 13:22:15 +0800

    Update info-gcbot2.js

--- plugins/info-gcbot2.js
+++ plugins/info-gcbot2.js
@@ -2,9 +2,9 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.wm
 let str = `
-✧─────[ *Group TokoBot* ]─────✧
+✧─────[ *Group Random Botz* ]─────✧
 🍀 Group 2 :
-https://chat.whatsapp.com/Bb245eWe2OCJy4TgUpdzQg
+https://chat.whatsapp.com/Io11BFEwL2RAzIQBupUlAZ
 ✧──────────···──────────✧
 `.trim()
 conn.sendButton(m.chat, str, `${botol}`, [['🎀 OKE','Ok']],m)
